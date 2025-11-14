d02da4bb2f4277f6f64b8e76a4256f9b6fac9dee
Abdul Waleed <2312272@LAB.SZABIST.NET.PK>
2025-11-14 11:31:42 +0500

    MVC Created Products

---
+++ 14-nov/Models/DatabaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+
+namespace Models
+{
+
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ 14-nov/Models/Entities/Product.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Models.Entities
+{
+    [Table("Products")]
+    public class Product
+    {
+        [Column("product_id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ProductId { get; set; }
+
+        [Column("product_name")]
+        [Display(Name = "Product Name")]
+        public string ProductName { get; set; }
+        
+        [Column("price")]
+        [Display(Name = "Price")]
+        public int Price { get; set; }
+
+        [Column("description")]
+        [Display(Name = "Description")]
+        public string Description { get; set; }
+
+        [Column("quantity")]
+        [Display(Name = "Quantity")]
+        public int Quantity { get; set; }
+
+        [Column("ratings")]
+        [Display(Name = "Ratings")]
+        public string Ratings { get; set; }
+    }
+}
