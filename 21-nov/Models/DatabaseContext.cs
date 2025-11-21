fe3a1559c536fd90b74cb06f6a53f03a81988bdb
Abdul Waleed <2312272@LAB.SZABIST.NET.PK>
2025-11-21 11:14:45 +0500

    Added Category

---
+++ 21-nov/Models/DatabaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Models.Entities;
+using Microsoft.EntityFrameworkCore;
+using Technova_ecom.Models.Entities;
+
+
+
+namespace Models
+{
+
+    public class DatabaseContext : DbContext
+    {
+        public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ 21-nov/Models/Entities/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace Models.Entities
+{
+    [Table("Customers")]
+    public class Customer
+    {
+        [Column("customer_id")]
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CustomerId { get; set; }
+        [Column("first_name")]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+        [Column("last_name")]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Column("email")]
+        [Display(Name = "Email")]
+        
+        public string Email { get; set; }
+        [Column("password")]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+        [Column("address")]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+        [Column("phone_number")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+    }
+}
