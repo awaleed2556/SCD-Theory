96b6d94d3dd8318d0c4151f989470fe9c3b4bc9a
Abdul Waleed <2312272@LAB.SZABIST.NET.PK>
2025-11-28 11:55:52 +0500

    Added Categories fully

---
+++ 28-nov/Models/Entities/Category.cs
@@ -0,0 +1,23 @@
+﻿using Models.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Technova_ecom.Models.Entities
+{
+    [Table("Category")]
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("category_id")]
+        public int CategoryId { get; set; }
+
+        [Column("category_name")]
+        public string CategoryName { get; set; }
+
+        [Column("category_order")]
+        public int DisplayOrder { get; set; }
+    
+        public ICollection<Product>? Products { get; set; }
+    }
+}
