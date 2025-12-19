68f8213ae0a1834cc270a16e58dc9285fb869b0a
Abdul Waleed <2312272@LAB.SZABIST.NET.PK>
2025-12-19 12:39:47 +0500

    Added Proper Auth

---
+++ 19-dec/Models/Entities/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Technova_ecom.Models.Entities
+{
+    [Table("User")]
+    public class User
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("id")]
+        public int Id { get; set; }
+        [Column("user_name")]
+        public string UserName { get; set; }
+        [Column("hashed_password")]
+        public string HashedPassword { get; set; }
+        [Column("role")]
+        public string? Role { get; set; }
+    }
+}
